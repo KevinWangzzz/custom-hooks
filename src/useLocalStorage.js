47a4f378b8bee304db3d9c541c13ca7cfe0b9863
KevinWang <kevinwang2020@sina.com>
2023-01-27 23:20:04 +0800

    Add useLocalStorage method

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function useLocalStorage() {
-  const [key, set] = useState();
-  
-  return 
-   
+export default function useLocalStorage(key, defaultValue) {
+    const [value, setValue] = useState(() => {
+        const cachedValue = window.localStorage.getItem(key);
+        if (cachedValue !== null) return JSON.parse(cachedValue);
 
+        return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
+    });
+
+    useEffect(() => {
+        if (value === undefined) window.localStorage.removeItem(key);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+
+    return [value, setValue];
 }
